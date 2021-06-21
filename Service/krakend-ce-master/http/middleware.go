03d665ea5e16be118655507b04827950ee5265b4
jelena-vlajkov <vlajkovj31@gmail.com>
2021-06-21 17:33:45 +0200

    :whale: Docker enabling

--- Service/krakend-ce-master/http/middleware.go
+++ Service/krakend-ce-master/http/middleware.go
@@ -24,8 +24,11 @@ const cookie_maxAge = 604800000
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
-		c.Header("Access-Control-Allow-Origin", "https://localhost:4200")
+		if os.Getenv("DOCKER_ENV") == "" {
+			c.Header("Access-Control-Allow-Origin", "https://localhost:4200")
+		} else {
+			c.Header("Access-Control-Allow-Origin", "http://localhost:4200")
+		}
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
