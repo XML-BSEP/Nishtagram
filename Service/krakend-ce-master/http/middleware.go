e6ced990da3f5a75702055b71178f2c1e5625461
Bojan Vujic <58082605+VujicGit@users.noreply.github.com>
2021-07-07 06:21:46 +0200

    :wrench: updated config and docker compose files

--- Service/krakend-ce-master/http/middleware.go
+++ Service/krakend-ce-master/http/middleware.go
@@ -19,7 +19,7 @@ import (
 	"github.com/go-resty/resty/v2"
 )
 
-var annonymous_endpoints = []string{"/register", "/confirmAccount", "/getAll", "/getUserProfileById", "/isAllowedToFollow", "/resendRegistrationCode", "/resetPasswordMail", "/resetPassword", "/validateTotp", "/isTotpEnabled", "/getPostLocationsByLocationContaining", "/post/getPostByIdForSearch", "/searchUser", "/getPostsByTag", "/agent", "/agent/validate"}
+var annonymous_endpoints = []string{"/register", "/confirmAccount", "/getAll", "/getUserProfileById", "/isAllowedToFollow", "/resendRegistrationCode", "/resetPasswordMail", "/resetPassword", "/validateTotp", "/isTotpEnabled", "/getPostLocationsByLocationContaining", "/post/getPostByIdForSearch", "/searchUser", "/getPostsByTag", "/agent", "/agent/validate", "/room/:roomId", "/ws/:roomId", "/message/:receiver/:sender"}
 const cookie_maxAge = 604800000
 
 func CORSMiddleware() gin.HandlerFunc {
